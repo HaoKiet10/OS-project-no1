399b55efabdf2c5a161c16bc3fe1480198ece5f2
Thanh Dat <truongthanhdat0305@gmail.com>
2025-03-12 08:56:55 +0700

    Add rr and srtn

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include ".\Alogirithm\fcfs.cpp"
-#include ".\Alogirithm\sjf.cpp"
+#include ".\Alogirithm\ThanhDat\fcfs.cpp"
+#include ".\Alogirithm\ThanhDat\sjf.cpp"
 
 int main(int argc, char *argv[]) {
     const char* inputPath = argv[1];
     const char* outputPath = argv[2];
-    fcfs(inputPath, outputPath);
-    // sjf(inputPath, outputPath);
+    if (inputPath == NULL) inputPath = "input.txt"; // Default input file
+    if (outputPath == NULL) outputPath = "output.txt"; // Default output file
+    ifstream readStream(inputPath);
+    int option;
+
+    if (!readStream.is_open()) {
+        cout << "Error: File not found" << endl;
+        return 1;
+    }
+    readStream >> option;
+    
+    if (option == 1) fcfs(inputPath, outputPath);
+    else if (option == 2);
+    else if (option == 3) sjf(inputPath, outputPath);
+    else if (option == 4);
+    else cout << "Error: Invalid option" << endl;
     return 0;
 }
